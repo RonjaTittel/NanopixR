9be2aa3474619555fd0bf270baa1367d9a711b4e
Ronja Tittel <ronja.tittel@web.de>
2026-01-12 15:34:51 +0100

    function : normalize_name

---
+++ R/normalize_name.R
@@ -0,0 +1,24 @@
+#' Normalizing a file name
+#'
+#' Extracting the base name from a file path,
+#' Converting it to lower case and
+#' Removing the file extension.
+#'
+#' @param path_or_name Character string containing a file name or a file path
+#' @return A normalized, lowercase file name without extensions
+#' @export
+#'
+#' @examples
+#' normalize_name("This_Is_A_file_NAME_01.PNG")
+#'
+#' normalize_name("path/to/data/This_Is_A_file_NAME_02.PNG")
+#'
+normalize_name <- function(path_or_name) {
+  if(!is.character(path_or_name) || length(path_or_name) != 1) {
+    stop("path_or_name must be a single character string", call. = FALSE)
+  }
+
+  x <- basename(path_or_name)
+  x <- tolower(x)
+  tools::file_path_sans_ext(x)
+}
