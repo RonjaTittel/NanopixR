911b7269fb47f01184b26b116082f93506356687
Ronja Tittel <ronja.tittel@web.de>
2026-01-14 10:07:34 +0100

    utils: normalize_name

--- R/utils_normalize_name.R
+++ R/utils_normalize_name.R
@@ -1,12 +1,5 @@
-#' Normalizing a file name
-#'
-#' Extracting the base name from a file path,
-#' Converting it to lower case and
-#' Removing the file extension.
-#'
-#' @param path_or_name Character string containing a file name or a file path
-#' @return A normalized, lowercase file name without extensions
-#'
+# Normalizing file names to a consistent and comparable format
+
 .normalize_name <- function(path_or_name) {
   if(!is.character(path_or_name)) {
     stop("path_or_name must be a single character vector", call. = FALSE)
