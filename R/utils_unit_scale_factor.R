8557e19043a9a53f9ae3cb487342260abbba088b
Ronja Tittel <ronja.tittel@web.de>
2026-01-21 13:34:20 +0100

    Clean up

--- R/utils_unit_scale_factor.R
+++
@@ -1,9 +0,0 @@
-# Getting the factor according to the scale for unit conversion
-.unit_scale_factor <- function(unit) {
-  if (is.na(unit) || !nzchar(unit)) return(NA_real_)
-  u <- tolower(unit)
-  if (u == "nm")  return(1e-6)
-  if (u %in% c("um", "\u00b5m")) return(1e-3)
-  if (u == "mm")  return(1)
-  return(NA_real_)
-}
